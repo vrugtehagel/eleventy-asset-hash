feb8bce82e8d4b3554f196737848d8ed1632fe0a
vrugtehagel <vrugtehagel@pm.me>
2024-06-20 17:47:07 +0200

    Exclude equals symbols from URL regex

--- src/detect-assets.ts
+++ src/detect-assets.ts
@@ -1,12 +1,12 @@
-/** Valid URL path characters are [!$%(-;=@-[\]_a-z~].
+/** Valid URL path characters are [!$%(-;@-[\]_a-z~].
  * The entire regex matches assets in a string of content.
  * This does not match URLs including a domain, by design. */
 const regex = new RegExp(
-  "(?<=[^!$%(-;=@-[\\]_a-z~])" + // must be preceded by non-URL character
+  "(?<=[^!$%(-;@-[\\]_a-z~])" + // must be preceded by non-URL character
     "\\.{0,2}/" + // then zero to two periods and a slash
-    "[!$%(-;=@-[\\]_a-z~]*" + // any number of URL characters
+    "[!$%(-;@-[\\]_a-z~]*" + // any number of URL characters
     "\\.\\w+" + // the file extension, minimum one character long
-    "(?=[^!$%(-;=@-[\\]_a-z~])", // followed by a non-URL character
+    "(?=[^!$%(-;@-[\\]_a-z~])", // followed by a non-URL character
   "g", // This is not part of the regex, it's just a flag
 );
 
